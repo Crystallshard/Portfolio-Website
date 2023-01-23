516246d65c802863fa1326dd431b5d62da1b4e08
kishon ennis-norman <kennis-norman@senecacollege.ca>
2023-01-22 22:21:04 -0500

    updates

---
+++ src/components/contactMenu/contactMenu.js
@@ -0,0 +1,56 @@
+import * as React from "react"
+import {
+    contactMenuContainer,
+    contactMenuItem
+} from "../contactMenu/ContactMenuStyles.module.css"
+import { StaticImage} from "gatsby-plugin-image";
+import { Link } from "gatsby";
+
+const ContactMenu = () => {
+    const IMGSIZE = 50;
+
+    return (
+        <>
+            <ul className={contactMenuContainer}>
+                <li className={contactMenuItem}>
+                    <Link to="#">
+                        <StaticImage
+                            alt="Star"
+                            src="D:\Portfolio\portfolio\src\images\email.png"
+                            placeholder="blurred"
+                            layout="fixed"
+                            width={IMGSIZE}
+                            height={IMGSIZE}
+                        />
+                    </Link>
+                </li>
+                <li className={contactMenuItem}>
+                    <Link to="#">
+                        <StaticImage
+                            alt="Star"
+                            src="D:\Portfolio\portfolio\src\images\gatsby.png"
+                            placeholder="blurred"
+                            layout="fixed"
+                            width={IMGSIZE}
+                            height={IMGSIZE}
+                        />
+                    </Link>
+                </li>
+                <li className={contactMenuItem}>
+                <Link to="#">
+                    <StaticImage
+                        alt="Star"
+                        src="D:\Portfolio\portfolio\src\images\linkedin.png"
+                        placeholder="blurred"
+                        layout="fixed"
+                        width={IMGSIZE}
+                        height={IMGSIZE}
+                    />
+                </Link>
+                </li>
+            </ul>
+        </>
+    )
+}
+
+export default ContactMenu;
---
+++ src/components/navigationLinks/navigationLinks.js
@@ -0,0 +1,66 @@
+import * as React from "react"
+import {
+    NavigationLinksContainer,
+    NavigationLinksItems,
+    star
+} from "../navigationLinks/NavigationLinksStyles.module.css"
+import { StaticImage} from "gatsby-plugin-image";
+import { Link } from "gatsby";
+
+const NavigationLinks = () => {
+    const STARSIZE = 15;
+
+    return (
+        <>
+            <ul className={NavigationLinksContainer}>
+                <li className={NavigationLinksItems}>
+                    <div className={star}>
+                        <StaticImage
+                            alt="Star"
+                            src="D:\Portfolio\portfolio\src\images\Star.png"
+                            placeholder="blurred"
+                            layout="fixed"
+                            width={STARSIZE}
+                            height={STARSIZE}
+                        />
+                    </div>
+                    <Link to="#">
+                        <p>ABOUT</p>
+                    </Link>
+                </li>
+                <li className={NavigationLinksItems}>
+                    <div className={star}>
+                        <StaticImage
+                            alt="Star"
+                            src="D:\Portfolio\portfolio\src\images\Star.png"
+                            placeholder="blurred"
+                            layout="fixed"
+                            width={STARSIZE}
+                            height={STARSIZE}
+                        />
+                    </div>
+                    <Link to="#">
+                        <p>PROJECTS</p>
+                    </Link>
+                </li>
+                <li className={NavigationLinksItems}>
+                    <div className={star}>
+                        <StaticImage
+                            alt="Star"
+                            src="D:\Portfolio\portfolio\src\images\Star.png"
+                            placeholder="blurred"
+                            layout="fixed"
+                            width={STARSIZE}
+                            height={STARSIZE}
+                        />
+                    </div>
+                    <Link to="#">
+                        <p>RESUME</p>
+                    </Link>
+                </li>
+            </ul>
+        </>
+    )
+}
+
+export default NavigationLinks;
