f310239f61af7ea85ab8c6bf40e1c52cd6739b36
kishon ennis-norman <kennis-norman@senecacollege.ca>
2023-01-25 21:43:46 -0500

    New footer component and more text

---
+++ src/components/footer/footer.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+import {
+
+} from "../footer/footer.module.css"
+import { StaticImage} from "gatsby-plugin-image";
+import { Link } from "gatsby";
+
+const Footer = () => {
+    
+
+    return (
+        <>
+            {/* <div>test</div> */}
+        </>
+    )
+}
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,16 @@ import { Link } from "gatsby";
 import * as React from "react"
 import ContactMenu from "../components/contactMenu/contactMenu"
 import NavigationLinks from "../components/navigationLinks/navigationLinks"
+import Footer from "../components/footer/footer";
 import {
   main, 
   Stars,
   contentContainer,
-  content,
-  welcome
+  firstName,
+  welcome,
+  aboutContainer,
+  aboutText,
+  about
 } from "./indexStyles.module.css"
 
 const IndexPage = () => {
@@ -27,13 +31,23 @@ const IndexPage = () => {
           />
         </Link>
         
-      
+      <Footer />
       <NavigationLinks />
       <ContactMenu />
       <div className={contentContainer}>
-          <h3 className={welcome}>WELCOM, MY NAME IS</h3>
-          <h1 className={content}>KISHON ENNIS-NORMAN</h1>
-        </div>
+          <h3 className={welcome}>WELCOME, MY NAME IS</h3>
+          <h1 className={firstName}>KISHON ENNIS-NORMAN</h1>
+      </div>
+
+      <div className={aboutContainer}>
+        <h3 className={about}>ABOUT</h3>
+        <h1 className={aboutText}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </h1>
+      </div>
     </main>
   )
 }
