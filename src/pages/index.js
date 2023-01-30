1f1413549b4cd8680315aab133b6b963b1d35d3e
kishon ennis-norman <kennis-norman@senecacollege.ca>
2023-01-30 00:56:15 -0500

    new font, added image and repositioned container hight

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,12 +13,15 @@ import {
   welcome,
   aboutContainer,
   aboutText,
-  about
+  about,
+  aboutList,
+  portContainer
 } from "./indexStyles.module.css"
 
 const IndexPage = () => {
 
   const STARSSIZE = 100;
+  const PORTSIZE = 350;
 
   
   return (
@@ -44,13 +47,29 @@ const IndexPage = () => {
       </div>
 
       <div className={aboutContainer}>
-        <h3 className={about}>ABOUT</h3>
-          <h1 className={aboutText}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </h1>
+        <h1 className={about}>ABOUT</h1>
+        <ul className={aboutList}>
+          <li>
+            <h1 className={aboutText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </h1>
+          </li>
+          <li>
+            <div className={portContainer}>
+              <StaticImage
+                alt="Star"
+                src="D:\Portfolio\portfolio\src\images\git-logo.png"
+                placeholder="blurred"
+                layout="fixed"
+                width={PORTSIZE}
+                height={PORTSIZE}
+              />
+            </div>
+          </li>
+        </ul>
       </div>
     </main>
   )
